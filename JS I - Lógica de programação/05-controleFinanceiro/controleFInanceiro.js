85509c398bd73ae7bc32bac6e55e87998477c2f4
Rodrigo Paulo Bauernfeind <ro.bauerr@gmail.com>
2023-08-01 08:14:37 -0300

    Exercicio do controle financeiro (do while) levemente modificado, colocado somente uma variavel de saldo para que o mesmo seja atualizado em todas as telas, trazendo o sempre o saldo atual do usuário.

--- JS I - Lógica de programação/05-controleFinanceiro/controleFInanceiro.js
+++ JS I - Lógica de programação/05-controleFinanceiro/controleFInanceiro.js
@@ -1,11 +1,10 @@
-let saldoInicial = parseFloat(prompt("Defina um valor para o saldo inicial da conta: "))
+let saldo = parseFloat(prompt("Informe o saldo da conta: "))
 let opcaoSelecionada = 0
-let saldoAtual = 0
 let quantidadeAdicionada = 0
 let quantidadeRetirada = 0
 
 do {
-    opcaoSelecionada = parseFloat(prompt("O Saldo é R$ " + saldoInicial + "\n" +
+    opcaoSelecionada = parseFloat(prompt("O Saldo é R$ " + saldo + "\n" +
         "Selecione uma das opções abaixo para prosseguir: " + "\n" +
         "1 - Realizar um depósito" + "\n" +
         "2 - Realizar um saque" + "\n" +
@@ -15,13 +14,13 @@ do {
     switch (opcaoSelecionada) {
         case 1:
             quantidadeAdicionada = parseFloat(prompt("Qual valor deseja adicionar:"))
-            saldoAtual = parseFloat((saldoInicial) + (quantidadeAdicionada))
-            alert("Saldo atual: R$" + saldoAtual)
+            saldo = parseFloat((saldo) + (quantidadeAdicionada))
+            alert("Saldo atual: R$" + saldo)
             break;
         case 2:
             quantidadeRetirada = prompt("Qual valor deseja retirar")
-            saldoAtual = parseFloat(saldoInicial - quantidadeRetirada)
-            alert("Saldo atual: R$" + saldoAtual)
+            saldo = parseFloat(saldo - quantidadeRetirada)
+            alert("Saldo atual: R$" + saldo)
             break;
         case 3:
             alert("Encerrando a aplicação.")
